f1ecf6402e8afbcd4d08179c86e86bcbb30773a7
蒋轲磊 <1091593632@qq.com>
2020-09-08 15:43:18 +0800

    commit

--- 01KNN/sklearn_knn.py
+++ 01KNN/sklearn_knn.py
@@ -1,8 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+# import matplotlib.pyplot as plt
+# from matplotlib.colors import ListedColormap
 from sklearn import neighbors, datasets
+from sklearn.model_selection import train_test_split
 
+# 分类个数为 3
 n_neighbors = 3
 
 # 导入鸢尾花数据
@@ -10,36 +12,21 @@ iris = datasets.load_iris()
 X = iris.data[:, :2] # 取前两行
 y = iris.target # 分类值
 
-# 步长
-h = 0.02
+# 划分训练测试集
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-
-# 创建彩色的图
-cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 for weights in ['uniform', 'distance']:
 	# 创建 knn 分类器
 	clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
-	clf.fit(X, y)
+	clf.fit(x_train, y_train)
 	
-	x_min, x_max = min(X[:, 0]) - 1, max(X[:, 0]) + 1
-	y_min, y_max = min(X[:, 1]) - 1, max(X[:, 1]) + 1
-	xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-	                     np.arange(y_min, y_max, h))
-	Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 	
-	# 彩图
-	Z = Z.reshape(xx.shape)
-	plt.figure()
-	plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
-	
-	# 绘制训练点
-	
-	plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
-	plt.xlim(xx.min(), xx.max())
-	plt.ylim(yy.min(), yy.max())
-	plt.title("3-Class classification (k = %i, weights = '%s')"
-	          % (n_neighbors, weights))
+	Z = clf.predict(x_test)
+	print('-------------------------------------')
+	print(weights+'方法:')
+	print('实际分类情况')
+	print(y_test)
+	print('knn分类情况')
+	print(Z)
 
-plt.show()
